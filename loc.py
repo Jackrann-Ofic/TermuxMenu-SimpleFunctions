497e93b71a42a174e59b4f61f51b4eb0fa314881
Jackrann <113806348+Jackrann-Ofic@users.noreply.github.com>
2024-07-24 14:22:59 -0300

    Create loc.py

---
+++ loc.py
@@ -0,0 +1,4 @@
+maior_valor_de_latitude = #Coloca aqui oque literalmente a variável tá pedindo
+menor_valor_de_latitude = #Coloca aqui oque literalmente a variável tá pedindo
+maior_valor_de_longitude = #Coloca aqui oque literalmente a variável tá pedindo
+menor_valor_de_longitude = #Coloca aqui oque literalmente a variável tá pedindo
